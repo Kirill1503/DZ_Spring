980e93620b2f9dc9546336b6c4f70f77163e4e88
Kirill <K.katyshev1503@gmail.com>
2022-11-24 17:20:07 +0300

    FirstCommit

---
+++ src/main/java/com/example/dz_spring/DzSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dz_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dz_spring/DzSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
